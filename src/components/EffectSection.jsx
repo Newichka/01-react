61026e004de12358f8d8a635a761fbd02e66ddad
Newi <sholokhov69505rus@mail.ru>
2024-09-10 13:25:26 +0500

    end

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState, useCallback } from 'react'
 import Button from './Button/Button'
 import Modal from './Modal/Modal'
 
@@ -5,21 +6,46 @@ import Modal from './Modal/Modal'
 
 export default function EffectSection(){
     const [modal, setModal] = useState(false)
-
+    const [loading, setLoading] = useState(false)
+    const [users, setUsers] = useState([])
     function openModal(){
         setModal(true)
     }
+
+    const fetchUsers = useCallback(async () => {
+        setLoading(true)
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const users = await response.json()
+        setUsers(users)
+        setLoading(false)
+    },[])
+
+
+    useEffect(()=>{
+        fetchUsers()
+    }, [fetchUsers])
     
     return(
         <section>
             <h3>Effects</h3>
 
-            <Button onClick={openModal}>Открыть инфо</Button>
+            <Button style={{marginBottom: '1rem'}} onClick={openModal}>Открыть инфо</Button>
 
             <Modal open={modal}>
                 <h3>Hello from modal</h3>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui aliquid magni labore obcaecati consequatur culpa ea harum! Deleniti nobis porro ipsam magnam harum quod neque error omnis eos suscipit. Atque.</p>
+                <Button onClick={()=>setModal(false)}>CLose modal</Button>
             </Modal>
+
+            {loading && <p>Загрузка</p>}
+
+            {!loading && (
+                <ul>
+                    {users.map((user) => (
+                        <li key={user.id}> {user.name} </li>
+                    ))}
+                </ul>
+            )} 
         </section>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from '/logo-name.svg'
 
 export default function Header(){
   const[now, setNow] = useState(new Date())
-  setInterval(() => setNow (new Date()), 1000)
+
+
+  useEffect(()=>{
+    const interval = setInterval(() => setNow (new Date()), 1000)
+
+    return()=>{
+      clearInterval(interval)
+    }
+  }, [])
 
   return(
     <header>
